dd16565559921548f7690df108f99a9030832b15
Mezea11 <christiancastellanosmeza@gmail.com>
2023-12-30 23:48:53 +0100

    Code formatted and randomizer added to cards.

--- script.js
+++ script.js
@@ -1,86 +1,52 @@
-// GLOBAL VARIABLES
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard];
-let sum = firstCard + secondCard;
-let hasBlackJack = false;
-let isAlive = false;
-let message = "";
-
-// DOM VARIABLES
+// 2. Use getRandomCard() to set the values of firstCard and secondCard
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let cards = [firstCard, secondCard]
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
 let messageEl = document.getElementById("message-el")
-let sumEl = document.getElementById("sum-el");
-let cardsEl = document.getElementById("cards-el");
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
+
+// 1. Create a function, getRandomCard(), that always returns the number 5
 
 function getRandomCard() {
-    
+    let randomCard = Math.floor(Math.random() * 11) + 2;
+    return randomCard;
 }
 
+
 function startGame() {
-    renderGame();
+    renderGame()
 }
 
 function renderGame() {
-    cardsEl.innerText = "Cards: " 
-
+    cardsEl.textContent = "Cards: "
     for (let i = 0; i < cards.length; i++) {
-        cardsEl.textContent += cards[i] + " ";
+        cardsEl.textContent += cards[i] + " "
     }
-
-    sumEl.innerText = "Sum: " + sum;
-
+    
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
-        message = "Do you want to draw a new card?";
-        console.log("Player has blackjack: " + hasBlackJack);
-        isAlive = !isAlive;
-        console.log("Player is alive: " + isAlive);
+        message = "Do you want to draw a new card?"
     } else if (sum === 21) {
-        message = "You have got blackjack!";
-        hasBlackJack = !hasBlackJack;
-        isAlive = !isAlive;
-        console.log("Player has blackjack: " + hasBlackJack);
-        console.log("Player is alive: " + isAlive);
+        message = "You've got Blackjack!"
+        hasBlackJack = true
     } else {
-        message = "You have lost.";
-        console.log("Player has blackjack: " + hasBlackJack);
-        console.log("Player is alive: " + isAlive);
+        message = "You're out of the game!"
+        isAlive = false
     }
-
-    messageEl.innerText = message;
-    console.log(message);
+    messageEl.textContent = message
 }
+
 
 function newCard() {
-    let newCard = getRandomCard();
-    sum += newCard
-    cards.push(newCard);
-    console.log(cards);
-    // Render the game after drawing a new card
-    renderGame();
+    // 3. Use the getRandomCard() to set the value of card
+    let card = getRandomCard();
+    sum += card
+    cards.push(card)
+    console.log(cards)
+    renderGame()
 }
-
-
-
-/* // PRACTICE 
-let player1Time = Math.floor(Math.random() * 11);
-let player2Time = Math.floor(Math.random() * 11);
-
-function getFastestRaceTime() {
-    if (player1Time > player2Time) {
-        console.log("PLAYER 1 WON");
-        return player1Time
-    } else if (player1Time < player2Time) {
-        console.log("PLAYER 2 WON");
-        return player2Time
-    } else {
-        return console.log("ITS A TIE.")
-    }
-}
- 
-function totalRaceTime() {
-    let raceTime = getFastestRaceTime();
-    console.log("This was the fastest racetime: " + raceTime +"s");
-    return player1Time + player2Time
-}
-
-totalRaceTime(); */
